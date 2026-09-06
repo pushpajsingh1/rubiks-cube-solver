98c5efa3caf1624eee2928b2747e7edf11247bbb
Pushpaj singh <pushpajsingh1@gmail.com>
2026-09-06 16:12:45 +0530

    chore: initialize Rubik's Cube solver

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Rubik's Cube Solver</h1>
+
+      <p>Your interactive cube solver</p>
+
+      <button>Scramble</button>
+      <button>Solve</button>
+      <button>Reset Cube</button>
+    </div>
+  )
+}
+
+export default App
